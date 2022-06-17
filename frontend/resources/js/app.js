502e9fcae219fabddae39275549bf4229748499b
Carlos André Martins da Silva <59931270+JovemCarlosTI@users.noreply.github.com>
2022-06-17 21:56:36 +0000

    Botão desativado em ponto-create quando conta pessoal for

--- frontend/resources/js/app.js
+++ frontend/resources/js/app.js
@@ -84,7 +84,9 @@ function checkPassword(){
   // const submit = document.getElementById('submit');
 }
 
-async function verifyEmpresarial() {
+async function verifyEmpresarial(id) {
+  const url = `/get-user/id/${id}`;
+	
 	const user = (await (await fetch(url)).json())[0];
 
 	if(user.perfil_id == 1) return true;
